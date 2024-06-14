7ac4395e731e97c524559ae89884903efc32d975
hpc <andy12836@gmail.com>
2024-06-14 14:49:58 +0800

    Lesson 2 Lecture 44

---
+++ Controllers/CategoryController.cs
@@ -0,0 +1,54 @@
+﻿using BulkyWeb.Data;
+using BulkyWeb.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.IdentityModel.Tokens;
+using System.Diagnostics;
+
+namespace BulkyWeb.Controllers
+{
+    public class CategoryController : Controller
+    {
+        private ApplicationDbContext _db;
+        public CategoryController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+        public IActionResult Index()
+        {
+            List<Category> objCategoryList = _db.Categories.ToList();
+            return View(objCategoryList);
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Create(Category category)
+        {
+            // validation
+            if(!category.Name.IsNullOrEmpty() && category.Name.ToLower() == category.DisplayOrder.ToString())
+            {
+                // "name" should match "asp-for" in the View
+                ModelState.AddModelError("name", "Category Name should not match Display Order");
+            }
+
+            // validation
+            if (!category.Name.IsNullOrEmpty() && category.Name.ToLower() == "test")
+            {
+                // The error message will be displayed only when "asp-validation-summary = all"
+                ModelState.AddModelError("", "\"Test\" is not a valid name");
+            }
+
+            if (ModelState.IsValid) { 
+                _db.Categories.Add(category);
+                _db.SaveChanges();
+                return RedirectToAction("index");
+            }
+            return View();
+        }
+
+
+    }
+}
