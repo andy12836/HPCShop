0fc4989531ef805de5673725c8e2adbf9739ff8e
hpc <andy12836@gmail.com>
2024-06-16 00:49:07 +0800

    Chapter 1 End. All functions and modules are packed in a single project.

--- Controllers/CategoryController.cs
+++ Controllers/CategoryController.cs
@@ -44,10 +44,73 @@ namespace BulkyWeb.Controllers
             if (ModelState.IsValid) { 
                 _db.Categories.Add(category);
                 _db.SaveChanges();
+                TempData["create"] = "Data created successfully!";
                 return RedirectToAction("index");
             }
             return View();
         }
+
+
+        // 參數categoryId會由 Index.cshtml中傳遞 使用 asp-route-categoryId="@obj.Id"
+        public IActionResult Edit(int? categoryId)
+        {
+            if(categoryId==null || categoryId == 0) { return NotFound(); }
+            
+            // there are three ways to search in datatbase
+            // the "find()" can be only used to find by its key
+            Category? category = _db.Categories.Find(categoryId);
+            // Category? category2 = _db.Categories.FirstOrDefault(u=>u.Id==categoryId);
+            // Category? category3 = _db.Categories.Where(u => u.Id == categoryId).FirstOrDefault();
+            
+            if(category == null) { return NotFound(); }
+
+            return View(category);
+        }
+
+        // 參數category物件的屬性會由 Edit.cshtml 傳遞，在 <form> 中使用標籤 <input asp-for="Id"/> 等建立屬性
+
+        [HttpPost]
+        public IActionResult Edit(Category category)
+        {
+            if (ModelState.IsValid)
+            {
+                _db.Categories.Update(category);
+                _db.SaveChanges();
+                TempData["edit"] = "Data edited successfully!";
+                return RedirectToAction("index");
+            }
+            return View();
+        }
+
+        // 參數categoryId2會由 Index.cshtml中傳遞 使用 asp-route-categoryId2="@obj.Id"
+
+        public IActionResult Delete(int? categoryId2)
+        {
+            if (categoryId2 == null || categoryId2 == 0) { return NotFound(); }
+
+
+            Category? category = _db.Categories.Find(categoryId2);
+
+
+            if (category == null) { return NotFound(); }
+
+            return View(category);
+        }
+
+        // 參數category物件的屬性會由 Delete.cshtml 傳遞，在 <form> 中使用標籤 <input asp-for="Id"/> 等建立屬性
+        [HttpPost, ActionName("Delete")]
+        public IActionResult DeletePost(int? categoryId2)
+        {
+            if (categoryId2 == null) return NotFound();
+            Category obj = _db.Categories.FirstOrDefault(u => u.Id == categoryId2);
+            if (obj == null) return NotFound();
+
+            _db.Categories.Remove(obj);
+            _db.SaveChanges();
+            TempData["delete"] = "Data deleted successfully!";
+            return RedirectToAction("index");
+        }
+
 
 
     }
